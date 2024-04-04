f97f8b8f97e88328518c7fe0c71dca5ca48721ca
semihkenan <altuntas.semih34@gmail.com>
2024-04-04 06:17:42 +0200

    2.day for SpringCore

--- school/src/main/java/be/vdab/school/MyRunner.java
+++ school/src/main/java/be/vdab/school/MyRunner.java
@@ -1,13 +1,24 @@
 package be.vdab.school;
 
+import be.vdab.school.leerlingen.LeerlingRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyRunner implements CommandLineRunner {
-    @Override
-    public void run(String... args) {
-        System.out.println("Welkom in de school");
+    private final LeerlingRepository leerlingRepository;
+
+    public MyRunner(LeerlingRepository leerlingRepository) {
+        this.leerlingRepository = leerlingRepository;
     }
 
+    @Override
+    public void run(String... args) {
+        try {
+            leerlingRepository.findAll()
+                    .forEach(leerling -> System.out.println(leerling.getVoornaam() +" "+ leerling.getFamilienaam()));
+        } catch (IllegalArgumentException ex) {
+            ex.printStackTrace(System.err);
+        }
+    }
 }
--- school/src/main/java/be/vdab/school/leerlingen/LeerlingRepository.java
+++ school/src/main/java/be/vdab/school/leerlingen/LeerlingRepository.java
@@ -1,10 +1,32 @@
 package be.vdab.school.leerlingen;
 
+import jdk.dynalink.linker.LinkerServices;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.EnumMap;
+import java.util.List;
 
 @Component
 public class LeerlingRepository {
-    public LeerlingRepository(){
-        System.out.println("LeerlingRepository");
+    //    public LeerlingRepository(){
+//        System.out.println("LeerlingRepository");
+//    }
+    public List<Leerling> findAll() {
+        try (var stream = Files.lines(Path.of("/data/leerlingen.csv"))) {
+            return stream
+                    .map(regel -> regel.split(","))
+                    .map(regelOnderdelen ->
+                            new Leerling(
+                                    Long.parseLong(regelOnderdelen[0]),
+                                    regelOnderdelen[1],
+                                    regelOnderdelen[2]))
+                    .toList();
+
+        } catch (IOException | ArrayIndexOutOfBoundsException | NumberFormatException ex) {
+            throw new IllegalArgumentException("LeerlingBestand bevat verkeerde data", ex);
+        }
     }
 }
